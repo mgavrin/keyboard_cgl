57912d4f30fd0eb7e1d25452ef9a4aaa414d2800
Miranda Gavrin <mirandagavrin@gmail.com>
2019-11-17 14:31:31 -0500

    More work on the update function

--- game_of_life.py
+++ game_of_life.py
@@ -15,18 +15,33 @@ key_map = {'esc': (0,0), 'f1': (0,1), 'f2': (0,2), 'f3': (0,3), 'f4': (0,4),
            'scrolllock': (0,14), 'pause': (0,15)}
 #fill the rest in later; skip numbers for e.g. the nonexistent key underneath delete
 
-def update_cell(x,y):
+def update_cell(r,c):
     ''' Calculates the value of a cell from the values of its neighbors.
-    x: the row of the cell to be updated.
-    y: the column of the cell to be updated.
+    r: the row of the cell to be updated.
+    c: the column of the cell to be updated.
+    Returns: a bool representing the new value of the cell.
     '''
-    neighbors = set((x-1, y-1), (x-1, y), (x-1, y+1),
-                    (x, y-1), (x, y+1),
-                    (x+1, y-1), (x+1, y), (x+1, y+1))
+    neighbors = set((r-1, c-1), (r-1, c), (r-1, c+1),
+                    (r, c-1), (r, c+1),
+                    (r+1, c-1), (r+1, c), (r+1, c+1))
     if !kWrapAround:
         neighbors = exclude_out_of_bounds_neighbors(neighbors)
+    living_neighbors = 0
+    for neighbor in neighbors:
+        if keyboard[neighbor[0]][neighbor[1]]:
+            living_neighbors += 1
+    if keyboard[r][c]:
+        return 2 <= living_neighbors <= 3
+    else:
+        return living_neighbors == 3
+    
 
 def exclude_out_of_bounds_neighbors(neighbors):
+    ''' Removes any neighbors that are outside the keyboard grid
+    from the set of neighbors.
+    Neighbors: the set of neighbors, possibly including out of bounds cells.
+    Returns: the trimmed set.
+    '''
     if x == 0:
         pass
     if y == 0:
@@ -35,3 +50,4 @@ def exclude_out_of_bounds_neighbors(neighbors):
         excluded = set((x+1, y-1), (x+1, y), (x+1, y+1))
     if y == kKeyboardHeight-1:
         excluded = set((x-1, y+1), (x, y+1), (x+1, y+1))
+    return neighbors - excluded
